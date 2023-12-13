279cea5a530d2418eac51852436e51bd40a0fafe
Jonathan Fogel <77300605+fogeljonathan@users.noreply.github.com>
2023-12-13 16:05:00 -0500

    Update EvilJL.jl
    added activatewidemode

--- src/EvilJL.jl
+++ src/EvilJL.jl
@@ -1,5 +1,30 @@
 module EvilJL
+"""
+    activateWideMode(filename; destructive=false)
 
-greet() = print("Hello World!")
+This function removes all line breaks from a given file, resulting in a single, very long line of code. Any single line comment in the original file will result in a broken mess, so just memorize and delete any documentation.
+
+Defaults to saving the file as a filename_wide.jl copy, but can be destructive with the optional second input.
+"""
+function activateWideMode(filename::String; destructive = false)
+
+    newfilename = replace(filename, ".jl"=> "_wide.jl")
+    if destructive
+        newfilename = filename
+    end
+
+    file = open(filename, "r")
+    data = read(file, String)
+    close(file)
+    
+    data = replace(data, "\r"=>";")
+
+    touch(newfilename)
+    file = open(newfilename,"w")
+    write(newfilename,data)
+    close(file)
+
+end
+export activateWidemode
 
 end # module EvilJL
