73e559a469f2e332a8f133b04bcbca4751f69c0e
Jonathan Fogel <77300605+fogeljonathan@users.noreply.github.com>
2023-12-14 11:46:51 -0500

    Update EvilJL.jl
    added comment changer function, implemented it into wide activator

--- src/EvilJL.jl
+++ src/EvilJL.jl
@@ -1,10 +1,41 @@
 module EvilJL
+
 """
-    activateWideMode(filename; destructive=false)
+refactorComments(data)
 
-This function removes all line breaks from a given file, resulting in a single, very long line of code. Any single line comment in the original file will result in a broken mess, so just memorize and delete any documentation.
+    This function takes read data and removes all single line comments, turning them all into multiline comments. Now that I think about it, this actually isn't that evil!
 
-Defaults to saving the file as a filename_wide.jl copy, but can be destructive with the optional second input.
+    All data is saved in the process, characters are simply added to encapsulate any comment into a multiline format.
+"""
+function refactorComments(data::String)
+    i = 1
+    while i <length(data)-1
+        if data[i] == '#' && data[i+1] != '=' && data[max(1,i-1)] != '='
+            data = data[begin:i]*"="*data[i+1:end]
+            i+=1
+            j = i
+            while j < length(data)
+                if data[j] in ['\n','\r']
+                    data = data[begin:j-1]*"=#"*data[j:end]
+                    break
+                end
+                j+=1
+            end
+            i+=1
+        else 
+            i+=1
+        end
+    end
+    return data
+end
+export refactorComments
+
+"""
+activateWideMode(filename; destructive=false)
+
+    This function removes all line breaks from a given file, resulting in a single, very long line of code.
+
+    Defaults to saving the file as a filename_wide.jl copy, but can be destructive with the optional second input.
 """
 function activateWideMode(filename::String; destructive = false)
 
@@ -17,7 +48,9 @@ function activateWideMode(filename::String; destructive = false)
     data = read(file, String)
     close(file)
     
+    data = refactorComments(data)
     data = replace(data, "\r"=>";")
+    data = replace(data, "\n"=>";")
 
     touch(newfilename)
     file = open(newfilename,"w")
